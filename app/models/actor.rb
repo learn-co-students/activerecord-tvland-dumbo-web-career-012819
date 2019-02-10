f03f3076794f57ca28cde78b791a12488ec2bd72
ch2isk4kos <ch2isk4kos@gmail.com>
2019-02-10 18:16:57 -0500

    Done.

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -1,3 +1,24 @@
 class Actor < ActiveRecord::Base
-  
+
+    has_many :characters
+    has_many :shows, through: :characters
+
+    # has_many :characters =
+    # def characters
+    #     Characters.all.find do |character|
+    #         character.actor_id == self.id
+    #     end
+    # end
+
+    def full_name
+        "#{first_name} #{last_name}"
+    end
+
+    def list_roles
+        # returns an array of all instances true for #characters
+        characters.map do |character|
+            "#{character.name} - #{character.show.name}"
+        end
+    end
+
 end
