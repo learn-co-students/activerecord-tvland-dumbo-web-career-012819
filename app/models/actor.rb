25696b984dfd219df951d7816e76fd375d4e5349
Nate <github email address>
2019-02-11 09:05:17 -0500

    tests passed

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -1,3 +1,15 @@
 class Actor < ActiveRecord::Base
-  
+  has_many :characters
+  has_many :shows, through: :characters
+
+  def full_name
+    "#{self.first_name} #{self.last_name}"
+  end
+
+  def list_roles
+    character = self.characters.collect {|c| c.name }.join
+    show = self.shows.map {|s| s.name }.join
+    "#{character} - #{show}"
+  end
+
 end
